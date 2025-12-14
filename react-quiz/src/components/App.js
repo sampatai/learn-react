86592236b818605d11e282c708a12c85646aa020
sampat1 <sampat.adhikari@gmail.com>
2025-12-14 10:26:11 +1000

    complete quiz

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -6,6 +6,10 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
 const initialState = {
   questions: [],
   // 'loading','error','ready','active','finished'
@@ -14,7 +18,10 @@ const initialState = {
 
   answer: null,
   points: 0,
+  highscore: 0,
+  secondsRemaining: null,
 };
+const Time_Limit = 30;
 function reducer(state, action) {
   switch (action.type) {
     case "dataReceived":
@@ -22,7 +29,11 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * Time_Limit,
+      };
 
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -37,17 +48,38 @@ function reducer(state, action) {
       };
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+
     default:
       throw new Error("Unknown action");
   }
 }
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
+  const maxPoints = questions.reduce((prev, cur) => prev + cur.points, 0);
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((response) => {
@@ -70,13 +102,35 @@ export default function App() {
 
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPoints={maxPoints}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPoints={maxPoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
         )}
       </Main>
     </div>
---
+++ react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,23 @@
+function FinishScreen({ points, maxPoints, highscore, dispatch }) {
+  const percentage = (points / maxPoints) * 100;
+  return (
+    <>
+      <p className="result">
+        You scored <strong>{points}</strong> out of {maxPoints} (
+        {Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+
+      <div>
+        <button
+          className="btn btn-ui"
+          onClick={() => dispatch({ type: "restart" })}
+        >
+          Restart quiz
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default FinishScreen;
